b2b27dd767315bb6faf581afcec6a9d7f9c1d6fc
akshshar <akshat31187@gmail.com>
2016-10-19 09:19:59 +0000

    Add YDK crud support

---
+++ resources/libraries/common/python/ydk/providers/netconf_provider.py
@@ -0,0 +1,94 @@
+# Copyright (c) 2016 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Library for YDK provider capabilities."""
+
+from time import time, sleep
+import os
+from ydk.providers import NetconfServiceProvider
+from robot.api import logger
+from robot.utils.asserts import assert_equal
+from resources.libraries.common.python.topology import Topology
+import pdb
+
+class YDKNetconfProvider(object):
+    """Contains methods for managing and using YDK provider connections to network devices"""
+
+    __existing_connections = {}
+
+    def __init__(self, proto='ssh'):
+        self._session = None
+        self._node = None
+        self._proto= proto 
+
+    def _node_hash(self, node, port):
+        """Get IP address and port hash from node dictionary.
+
+        :param node: Node in topology.
+        :param port: 
+        :type node: dict
+        :return: IP address and port for the specified node.
+        :rtype: int
+        """
+
+        return hash(frozenset([node['mgmt_ip'], port]))
+
+    def open_netconf_session(self, node):
+        """Connect to node using YDK's Netconf service provider.
+
+        """
+        self._node = node
+        netconf_port = Topology.get_netconf_port_from_node(node)
+        node_hash = self._node_hash(self, node, netconf_port)
+        if node_hash in YDKNetconfProvider.__existing_connections:
+            self._session = YDKNetconfProvider.__existing_connections[node_hash]
+            logger.debug('reusing netconf session: {0}'.format(self._session))
+        else:
+            start = time()
+            self._session =  NetconfServiceProvider(address=node['mgmt_ip'],
+                                                    port=netconf_port,
+                                                    username=node['username'],
+                                                    password=node['password'],
+                                                    protocol=self._proto)
+
+
+            YDKNetconfProvider.__existing_connections[node_hash] = self._session
+
+            logger.trace('connect took {} seconds'.format(time() - start))
+            logger.debug('new connection: {0}'.format(self._session))
+
+
+        logger.debug('Connections: {0}'.format(str(YDKNetconfProvider.__existing_connections)))
+        return self._session
+
+    def close_netconf_session(self, node):
+        """Close netconf connection to the node.
+
+        :param node: The node to disconnect from.
+        :type node: dict
+        """
+        node_hash = self._node_hash(node)
+        if node_hash in YDKNetconfProvider.__existing_connections:
+            logger.debug('Disconnecting peer: {}, {}'.
+                         format(node['host'], node['port']))
+            ssh = YDKNetconfProvider.__existing_connections.pop(node_hash)
+
+        self._session.close()    
+
+    def _reconnect(self):
+        """Close the SSH connection and open it again."""
+
+        node = self._node
+        self.close_netconf_session(node)
+        self.start_netconf_session(node)
+
---
+++ resources/libraries/common/python/ydk/services/crud_service.py
@@ -0,0 +1,115 @@
+# Copyright (c) 2016 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Library for SSH connection management."""
+
+from time import time, sleep
+import os
+from ydk.services import CRUDService
+from robot.api import logger
+from robot.utils.asserts import assert_equal
+from resources.libraries.common.python.ydk.providers.netconf_provider import YDKNetconfProvider
+import pdb
+
+class YDKCrudService(object):
+    """Contains methods for sending create/read/update/delete operations over a YDK provider"""
+
+    __existing_connections = {}
+
+    def __init__(self):
+        self._session = None
+        self._crud = CRUDService()
+
+
+    def _open_provider_session(self, node, protocol="netconf", provider_proto='default'):
+
+        def netconf():
+            """
+            Return the YDK netconf provider 
+            """
+            if provider_proto == 'default':
+               netconf_proto = 'ssh'
+            else:
+               netconf_proto = provider_proto
+
+            provider =  YDKNetconfProvider(netconf_proto)
+            self._session = provider.open_netconf_session(node)
+            return self._session
+
+        def grpc():
+            ##TODO: when grpc gets implemented by YDK as a provider.
+            return None
+
+
+        provider_dict = {'netconf': netconf(),
+                         'grpc': grpc()}
+
+
+        return provider_dict[protocol] 
+
+    def create(self, entity, node, protocol="netconf", provider_proto='default'):
+        """Invoke a crud.create for a given entity on a node using the specified protocol.
+
+        """
+
+        # Open up the provider session if necessary
+        self._open_provider_session(node, protocol, provider_proto)        
+       
+        # Initiate a crud.create on the selected provider
+
+        self._crud.create(self._session, entity)
+
+        logger.debug('Crud Create Successful')
+
+    def read(entity, node, protocol="netconf", provider_proto='default'):
+        """Invoke a crud.create for a given entity on a node using the specified protocol.
+
+        """
+
+        # Open up the provider session if necessary
+        self._open_provider_session(node, protocol, provider_proto)          
+
+        # Initiate a crud.create on the selected provider
+
+        self._crud.read(self._session, entity)
+
+        logger.debug('Crud Read Successful')
+
+
+    def update(entity, node, protocol="netconf", provider_proto='default'):
+        """Invoke a crud.create for a given entity on a node using the specified protocol.
+
+        """
+
+        # Open up the provider session if necessary
+        self._open_provider_session(node, protocol, provider_proto)
+
+        # Initiate a crud.create on the selected provider
+
+        self._crud.update(self._session, entity)
+
+        logger.debug('Crud Update Successful')
+
+    def delete(entity, node, protocol="netconf", provider_proto='default'):
+        """Invoke a crud.create for a given entity on a node using the specified protocol.
+
+        """
+
+        # Open up the provider session if necessary
+        self._open_provider_session(node, protocol, provider_proto)
+
+        # Initiate a crud.create on the selected provider
+
+        self._crud.delete(self._session, entity)
+
+        logger.debug('Crud Delete Successful')
